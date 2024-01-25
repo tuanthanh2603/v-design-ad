a0ee9c1ee40005e6f58c8744c940cceb91cb4fbf
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2024-01-25 18:20:23 +0700

    admin

--- resources/views/admin/layout-admin.blade.php
+++ resources/views/admin/layout-admin.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin - V Design</title>
+    <link rel="stylesheet" href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/soft-ui-dashboard.min.css?v=1.0.7">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 </head>
-<body>
-    
+<body class="g-sidenav-show bg-gray-100">
+    @include('name')
 </body>
 </html>
