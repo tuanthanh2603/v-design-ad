7a7bb9215e0e7db0aa6f61ec377bd0ec9ec9384e
Hoang Phuc Hau <phuchau.tea@gmail.com>
2024-01-26 19:48:43 +0700

    added migrations for models

---
+++ database/migrations/2024_01_26_123946_create_products_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('sku')->unique();
+            $table->string('slug')->unique();
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->string('materials')->nullable(); // chất liệu
+            $table->string('size')->nullable(); // kích thước
+            $table->string('shipping')->nullable(); // vận chuyển
+            $table->string('setup')->nullable(); // lắp đặt
+            $table->string('waranty')->nullable(); // bảo hành
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
