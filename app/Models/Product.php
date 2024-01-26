f697a9ced0b8da37e5ef0402cd953de67c641c43
Hoang Phuc Hau <phuchau.tea@gmail.com>
2024-01-26 19:29:51 +0700

    added Models

---
+++ app/Models/Product.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'sku',
+        'slug',
+        'description',
+        'image',
+        'materials', // chất liệu
+        'size', // kích thước
+        'shipping', // vận chuyển
+        'setup', // lắp đặt
+        'waranty', // bảo hành
+        'category_id'
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+}
