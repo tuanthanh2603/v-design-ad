53af2fa78d0fb171be06c05dfdb0eb78d25c49b7
tuanthanh-2 <tuanthanhktltk21@gmail.com>
2024-01-28 14:35:56 +0700

    update service

--- resources/views/user/pages/service/index2.blade.php
+++ resources/views/user/pages/service/index2.blade.php
@@ -6,4 +6,116 @@
         <h1 class="sec_title_2">Dịch vụ tại V Design</h1>
     </div>
 </section>
+<section class="section recruit_page_2">
+    <div class="grid-container">
+        <div class="flex_box jus_between al_start flex_wrap">
+            <div class="col_5 tablet_col_12">
+                <div class="heading">
+                    <h2 class="sec_title" style="color: #C5A47E">
+                            Thiết kế bảng hiệu                        </h2>
+                        <div class="fs_20 lh_24 fw400">
+                            Tư vấn và thiết kế bảng hiệu dựa trên yêu cầu và mục tiêu quảng cáo của khách hàng.                       </div>
+                                    </div>
+
+                    <div class="text_center">
+                        <div class="load_more_projects btn_secondary" onclick="window.location.href='{{ url('/lien-he') }}'">
+                            Liên hệ tư vấn                    </div>
+                    </div>
+
+            </div>
+            <div class="col_7 tablet_col_12">
+                <div class="img_wrap">
+                    <img width="700" height="300" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="763" height="352" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="" alt="" decoding="async" /></noscript>                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section recruit_page_2">
+    <div class="grid-container">
+        <div class="flex_box jus_between al_start flex_wrap">
+            <div class="col_5 tablet_col_12">
+                <div class="heading">
+                    <h2 class="sec_title" style="color: #C5A47E">
+                            In Ấn và Sản Xuất                       </h2>
+                        <div class="fs_20 lh_24 fw400">
+                            In ấn bảng hiệu sử dụng công nghệ và vật liệu chất lượng cao. Sản xuất bảng hiệu từ các vật liệu như nhôm, kính, acrylic, PVC, hay composite.                        </div>
+                                    </div>
+                <div class="text_center">
+                    <div class="load_more_projects btn_secondary" data-page="2" data-target="project_list" data-max="3">
+                        Liên hệ tư vấn                    </div>
+                </div>
+            </div>
+            <div class="col_7 tablet_col_12">
+                <div class="img_wrap">
+                    <img width="700" height="300" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="763" height="352" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="" alt="" decoding="async" /></noscript>                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section recruit_page_2">
+    <div class="grid-container">
+        <div class="flex_box jus_between al_start flex_wrap">
+            <div class="col_5 tablet_col_12">
+                <div class="heading">
+                    <h2 class="sec_title">
+                        Thi Công Lắp Đặt                       </h2>
+                        <div class="fs_20 lh_24 fw400">
+                            Lắp đặt bảng hiệu tại các vị trí quảng cáo chiến lược. Sử dụng kỹ thuật và công nghệ lắp đặt an toàn và chuyên nghiệp.                     </div>
+                                    </div>
+                <div class="text_center">
+                    <div class="load_more_projects btn_secondary" data-page="2" data-target="project_list" data-max="3">
+                        Liên hệ tư vấn                    </div>
+                </div>
+            </div>
+            <div class="col_7 tablet_col_12">
+                <div class="img_wrap">
+                    <img width="700" height="300" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="763" height="352" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="" alt="" decoding="async" /></noscript>                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section recruit_page_2">
+    <div class="grid-container">
+        <div class="flex_box jus_between al_start flex_wrap">
+            <div class="col_5 tablet_col_12">
+                <div class="heading">
+                    <h2 class="sec_title">
+                        Bảo Dưỡng và Sửa Chữa                       </h2>
+                        <div class="fs_20 lh_24 fw400">
+                            Cung cấp dịch vụ bảo dưỡng định kỳ để đảm bảo bảng hiệu luôn giữ được hiệu suất tốt nhất. Sửa chữa bảng hiệu khi có sự cố hoặc hỏng hóc.                       </div>
+                                    </div>
+                <div class="text_center">
+                    <div class="load_more_projects btn_secondary" data-page="2" data-target="project_list" data-max="3">
+                        Liên hệ tư vấn                    </div>
+                </div>
+            </div>
+            <div class="col_7 tablet_col_12">
+                <div class="img_wrap">
+                    <img width="700" height="300" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="763" height="352" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="" alt="" decoding="async" /></noscript>                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section recruit_page_2">
+    <div class="grid-container">
+        <div class="flex_box jus_between al_start flex_wrap">
+            <div class="col_5 tablet_col_12">
+                <div class="heading">
+                    <h2 class="sec_title">
+                        Quảng Cáo Ngoại Thất                       </h2>
+                        <div class="fs_20 lh_24 fw400">
+                            Thiết kế và sản xuất các sản phẩm quảng cáo ngoại thất như banner, hộp đèn, biển hộp, v.v.                       </div>
+                                    </div>
+                <div class="text_center">
+                    <div class="load_more_projects btn_secondary" data-page="2" data-target="project_list" data-max="3">
+                        Liên hệ tư vấn                    </div>
+                </div>
+            </div>
+            <div class="col_7 tablet_col_12">
+                <div class="img_wrap">
+                    <img width="700" height="300" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="763" height="352" src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View05-1.jpg" class="" alt="" decoding="async" /></noscript>                </div>
+            </div>
+        </div>
+    </div>
+</section>
 @endsection
