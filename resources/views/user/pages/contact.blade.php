adc4f9a1d617dbd2ef0052b8fadf2c43cda71dd7
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2024-01-28 11:49:18 +0700

    contact

--- resources/views/user/pages/contact.blade.php
+++ resources/views/user/pages/contact.blade.php
@@ -1,9 +1,181 @@
 @extends('user.layouts.master')
-@section('bodyClass', 'page-template page-template-templates page-template-projects page-template-templatesprojects-php page page-id-78 wp-custom-logo wp-embed-responsive right-sidebar nav-float-right separate-containers header-aligned-left dropdown-hover vsc-initialized using-mouse')
+@section('bodyClass',
+    'page-template page-template-templates page-template-projects page-template-templatesprojects-php
+    page page-id-78 wp-custom-logo wp-embed-responsive right-sidebar nav-float-right separate-containers header-aligned-left
+    dropdown-hover vsc-initialized using-mouse')
 @section('content')
-<section class="section project_page_1">
-    <div class="grid-container">
-        <h1 class="sec_title_2">Liên hệ</h1>
+    <div class="site grid-container container hfeed" id="page">
+        <div class="site-content" id="content">
+            <div id="banner_gr" class="banner_gr section">
+                <div
+                    class="swiper banner_slide swiper-fade swiper-initialized swiper-horizontal swiper-pointer-events swiper-watch-progress swiper-backface-hidden">
+                    <div class="swiper-wrapper" id="swiper-wrapper-1dc85d6cc73c21012" aria-live="off">
+                        <div class="swiper-slide swiper-slide-visible swiper-slide-active" role="group" aria-label="1 / 1"
+                            style="width: 1280px; opacity: 1; transform: translate3d(0px, 0px, 0px);">
+                            <div class="img_wrap">
+                                <img width="1830" height="650"
+                                    src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View01_auto_x2-1.jpg"
+                                    class="lazyloaded" alt="" decoding="async"
+                                    data-ll-status="loaded"><noscript><img width="1830" height="650"
+                                        src="https://sarchitects.com.vn/wp-content/uploads/2023/07/View01_auto_x2-1.jpg"
+                                        class="" alt="" decoding="async" /></noscript>
+                            </div>
+                        </div>
+                    </div>
+                    <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+                </div>
+                <div class="gr_head absolute left_cont">
+                    <div class="container">
+                        <h1>Liên hệ V Design AD</h1>
+                    </div>
+                </div>
+            </div>
+            <section class="section contact_page bg_cus rocket-lazyload lazyloaded"
+                style="background-image: url(&quot;https://sarchitects.com.vn/wp-content/uploads/2023/07/Slice-1.jpg&quot;);"
+                data-ll-status="loaded">
+                <div class="grid-container">
+                    <div class="flex_box jus_between al_start flex_wrap">
+                        <div class="col_5 tablet_col_12">
+                            <h2 class="sec_title">
+                                THÔNG TIN LIÊN HỆ </h2>
+
+                            <div class="branches">
+                                <div class="branch">
+                                    <div class="title flex_box al_center">
+                                        <svg width="15" height="20" viewBox="0 0 15 20" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M7.5 0C3.35937 0 0 3.35938 0 7.5C0 9.18945 0.556641 10.7422 1.49902 11.9971L7.50488 20L13.5059 11.9971C14.4482 10.7422 15 9.18945 15 7.5C15 3.35938 11.6406 0 7.5 0ZM7.5 12.5C4.73633 12.5 2.5 10.2588 2.5 7.5C2.5 4.73633 4.74121 2.5 7.5 2.5C10.2588 2.5 12.5 4.74121 12.5 7.5C12.5 10.2588 10.2637 12.5 7.5 12.5Z"
+                                                fill="black"></path>
+                                        </svg>
+                                        <span>
+                                            Văn phòng Hồ Chí Minh </span>
+                                    </div>
+                                    <div class="info fs_14 lh_20 fw400">
+                                        <p><strong>Địa chỉ</strong>: <a href="https://maps.app.goo.gl/y5kryVNPnupiwKuz8">23D
+                                                Miếu Gò Xoài, Bình Hưng Hoà A, Bình Tân, TP. Hồ Chí Minh</a><br>
+                                            <strong>Điện thoại</strong>: <a href="tel:0975353597">0975 3535 97</a><br>
+
+                                        </p>
+                                    </div>
+                                    <div class="title flex_box al_center">
+                                        <svg width="15" height="20" viewBox="0 0 15 20" fill="none"
+                                            xmlns="http://www.w3.org/2000/svg">
+                                            <path
+                                                d="M7.5 0C3.35937 0 0 3.35938 0 7.5C0 9.18945 0.556641 10.7422 1.49902 11.9971L7.50488 20L13.5059 11.9971C14.4482 10.7422 15 9.18945 15 7.5C15 3.35938 11.6406 0 7.5 0ZM7.5 12.5C4.73633 12.5 2.5 10.2588 2.5 7.5C2.5 4.73633 4.74121 2.5 7.5 2.5C10.2588 2.5 12.5 4.74121 12.5 7.5C12.5 10.2588 10.2637 12.5 7.5 12.5Z"
+                                                fill="black"></path>
+                                        </svg>
+                                        <span>
+                                            Xưởng sản xuất </span>
+                                    </div>
+                                    <div class="info fs_14 lh_20 fw400">
+                                        <p><strong>Địa chỉ</strong>: <a
+                                                href="https://maps.app.goo.gl/y5kryVNPnupiwKuz8">40/31 Miếu Gò Xoài, Bình
+                                                Hưng Hoà A, Bình Tân, TP. Hồ Chí Minh</a><br>
+                                            <strong>Điện thoại</strong>: <a href="tel:0855445533">0855 44 55 33</a><br>
+
+                                        </p>
+                                    </div>
+                                    <div class="relative map_wrap img_wrap">
+                                        <a href="https://maps.app.goo.gl/y5kryVNPnupiwKuz8" target="_blank"
+                                            class="wrap_link"></a>
+                                        <img width="574" height="160"
+                                            src="https://sarchitects.com.vn/wp-content/uploads/2023/07/map.jpg"
+                                            class="lazyloaded" alt="" decoding="async"
+                                            sizes="(max-width: 574px) 100vw, 574px"
+                                            srcset="https://sarchitects.com.vn/wp-content/uploads/2023/07/map.jpg 574w, https://sarchitects.com.vn/wp-content/uploads/2023/07/map-300x84.jpg 300w"
+                                            data-ll-status="loaded"><noscript><img width="574" height="160"
+                                                src="https://sarchitects.com.vn/wp-content/uploads/2023/07/map.jpg"
+                                                class="" alt="" decoding="async"
+                                                srcset="https://sarchitects.com.vn/wp-content/uploads/2023/07/map.jpg 574w, https://sarchitects.com.vn/wp-content/uploads/2023/07/map-300x84.jpg 300w"
+                                                sizes="(max-width: 574px) 100vw, 574px" /></noscript>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col_6 tablet_col_12">
+                            <div class="sec_title_2 mb_text_center">
+                                Gửi tin nhắn cho chúng tôi </div>
+                            <div class="fs_14 lh_22 fw400 mb_text_center">
+                                Quý khách có bất kỳ câu hỏi hay yêu cầu về dịch vụ, vui lòng điền vào bảng thông tin bên
+                                dưới để được hỗ trợ. </div>
+
+                            <div class="wpcf7 js" id="wpcf7-f5-o1" lang="en-US" dir="ltr">
+                                <div class="screen-reader-response">
+                                    <p role="status" aria-live="polite" aria-atomic="true"></p>
+                                    <ul></ul>
+                                </div>
+                                <form action="/lien-he-s-architects/#wpcf7-f5-o1" method="post" class="wpcf7-form init"
+                                    aria-label="Contact form" novalidate="novalidate" data-status="init">
+                                    <div style="display: none;">
+                                        <input type="hidden" name="_wpcf7" value="5">
+                                        <input type="hidden" name="_wpcf7_version" value="5.7.7">
+                                        <input type="hidden" name="_wpcf7_locale" value="en_US">
+                                        <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f5-o1">
+                                        <input type="hidden" name="_wpcf7_container_post" value="0">
+                                        <input type="hidden" name="_wpcf7_posted_data_hash" value="">
+                                    </div>
+                                    <div class="form_wrap flex_box flex_wrap jus_between">
+                                        <div class="col_6 mobile_col_12 relative form_group">
+                                            <p><label for="your-name">HỌ VÀ TÊN</label><span
+                                                    class="wpcf7-form-control-wrap" data-name="your-name"><input
+                                                        size="40"
+                                                        class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required"
+                                                        id="your-name" aria-required="true" aria-invalid="false"
+                                                        value="" type="text" name="your-name"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col_6 mobile_col_12 relative form_group">
+                                            <p><label for="your-phone">SỐ ĐIỆN THOẠI</label><span
+                                                    class="wpcf7-form-control-wrap" data-name="your-phone"><input
+                                                        size="40"
+                                                        class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel"
+                                                        id="your-phone" aria-required="true" aria-invalid="false"
+                                                        value="" type="tel" name="your-phone"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col_6 mobile_col_12 relative form_group">
+                                            <p><label for="your-email">EMAIL</label><span class="wpcf7-form-control-wrap"
+                                                    data-name="your-email"><input size="40"
+                                                        class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email"
+                                                        id="your-email" aria-required="true" aria-invalid="false"
+                                                        value="" type="email" name="your-email"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col_6 mobile_col_12 relative form_group">
+                                            <p><label for="your-address">ĐỊA CHỈ</label><span
+                                                    class="wpcf7-form-control-wrap" data-name="your-address"><input
+                                                        size="40" class="wpcf7-form-control wpcf7-text"
+                                                        id="your-address" aria-invalid="false" value=""
+                                                        type="text" name="your-address"></span>
+                                            </p>
+                                        </div>
+                                        <div class="col_12 relative form_group">
+                                            <p><label for="your-message">NỘI DUNG TIN NHẮN</label><span
+                                                    class="wpcf7-form-control-wrap" data-name="your-message">
+                                                    <textarea cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" id="your-message"
+                                                        aria-invalid="false" name="your-message"></textarea>
+                                                </span>
+                                            </p>
+                                        </div>
+                                        <div class="col_12 relative form_group submit_btn mb_text_center">
+                                            <p><input class="wpcf7-form-control has-spinner wpcf7-submit btn_submit"
+                                                    type="submit" value="GỬI TIN NHẮN"><span
+                                                    class="wpcf7-spinner"></span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="wpcf7-response-output" aria-hidden="true"></div>
+                                </form>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </section>
+
+
+
+        </div>
     </div>
-</section>
 @endsection
