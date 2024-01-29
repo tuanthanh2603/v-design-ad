74f90c132ba23104ec7dbf353dedf7d5f0a27cf7
tuanthanh-2 <tuanthanhktltk21@gmail.com>
2024-01-29 09:28:10 +0700

    update news

--- resources/views/user/pages/news/index.blade.php
+++ resources/views/user/pages/news/index.blade.php
@@ -1,9 +1,130 @@
 @extends('user.layouts.master')
-@section('bodyClass', 'page-template page-template-templates page-template-projects page-template-templatesprojects-php page page-id-78 wp-custom-logo wp-embed-responsive right-sidebar nav-float-right separate-containers header-aligned-left dropdown-hover vsc-initialized using-mouse')
+@section('bodyClass', 'page-template page-template-templates page-template-projects page-template-templatesprojects-php
+page page-id-78 wp-custom-logo wp-embed-responsive right-sidebar nav-float-right separate-containers header-aligned-left
+dropdown-hover vsc-initialized using-mouse')
 @section('content')
-<section class="section project_page_1">
+<div id="banner_gr" class="banner_gr section">
+    <div
+        class="swiper banner_slide swiper-fade swiper-initialized swiper-horizontal swiper-pointer-events swiper-watch-progress swiper-backface-hidden">
+        <div class="swiper-wrapper" id="swiper-wrapper-10efc78a41936f10cf" aria-live="off">
+            <div class="swiper-slide swiper-slide-visible swiper-slide-active" role="group" aria-label="1 / 1"
+                style="width: 1280px; opacity: 1; transform: translate3d(0px, 0px, 0px);">
+                <div class="img_wrap">
+                    <img width="1830" height="650"
+                        src="https://sarchitects.com.vn/wp-content/uploads/2023/07/lee-ferrell-UjX9YmFs_O4-unsplash-1.jpg"
+                        class="lazyloaded" alt="" decoding="async" data-ll-status="loaded"><noscript><img width="1830"
+                            height="650"
+                            src="https://sarchitects.com.vn/wp-content/uploads/2023/07/lee-ferrell-UjX9YmFs_O4-unsplash-1.jpg"
+                            class="" alt="" decoding="async" /></noscript>
+                </div>
+            </div>
+        </div>
+        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
+    </div>
+    <div class="gr_head absolute left_cont">
+        <div class="container">
+            <h1>XU HƯỚNG THIẾT KẾ</h1>
+        </div>
+    </div>
+</div>
+
+<section class="media_page section">
     <div class="grid-container">
-        <h1 class="sec_title_2">Xu hướng thiết kế</h1>
+        <div class="flex_box al_end jus_between tablet_flex_col">
+
+            <div class="col_12 tablet_col_12">
+                <div class="terms_menu">
+                    <ul id="menu-menu-media-2"
+                        class="ls_none m_0 flex_box al_center jus_end tablet_jus_center mobile_jus_between">
+                        <li id="menu-item-640"
+                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-640"><a
+                                href="https://sarchitects.com.vn/danh-muc-tin-tuc/xu-huong-thiet-ke/">Xu hướng thiết
+                                kế</a></li>
+                        <li id="menu-item-639"
+                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-639"><a
+                                href="https://sarchitects.com.vn/danh-muc-tin-tuc/tin-s-architects/">Tin S
+                                Architects</a></li>
+                        <li id="menu-item-641"
+                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-641"><a
+                                href="https://sarchitects.com.vn/danh-muc-tin-tuc/su-kien/">Sự kiện</a></li>
+                        <li id="menu-item-642"
+                            class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-642"><a
+                                href="https://sarchitects.com.vn/danh-muc-tin-tuc/thu-vien/">Thư viện</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="grid_box grid_2 box_hot_posts tablet_grid_1">
+            <div class="item relative">
+                <a href="https://sarchitects.com.vn/kts-doan-bang-giang-kts-can-hieu-va-nam-ro-yeu-to-ban-sac-ban-dia-trong-kien-truc-4/"
+                    class="wrap_link"></a>
+                <div class="img_wrap">
+                    <img width="902" height="508"
+                        src="https://sarchitects.com.vn/wp-content/uploads/2023/07/3443_20841_5921-1.jpg"
+                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image lazyloaded" alt=""
+                        decoding="async" data-ll-status="loaded"><noscript><img width="902" height="508"
+                            src="https://sarchitects.com.vn/wp-content/uploads/2023/07/3443_20841_5921-1.jpg"
+                            class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt=""
+                            decoding="async" /></noscript>
+                </div>
+                <div class="cont_wrap">
+                    <div class="date_cat flex_box al_center">
+                        <a href="https://sarchitects.com.vn/danh-muc-tin-tuc/video/">
+                            Video </a>
+                        <div class="date">
+                            12/07/2023 </div>
+                    </div>
+                    <h3 class="font_oswald fw300 limit_line limit_5 fs_24 lh_34 text_uppercase m_0">KTS Đoàn Bằng Giang:
+                        KTS cần hiểu và nắm rõ yếu tố bản sắc, bản địa trong kiến trúc</h3>
+                </div>
+            </div>
+            <div class="item relative">
+                <a href="https://sarchitects.com.vn/ky-nguyen-do-thi-va-nhung-buoc-chuyen-trong-nghien-cuu-do-thi-o-dong-nam-a-4/"
+                    class="wrap_link"></a>
+                <div class="img_wrap">
+                    <img width="840" height="510"
+                        src="https://sarchitects.com.vn/wp-content/uploads/2023/07/23A03023-768x516-1.jpg"
+                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image lazyloaded" alt=""
+                        decoding="async" data-ll-status="loaded"><noscript><img width="840" height="510"
+                            src="https://sarchitects.com.vn/wp-content/uploads/2023/07/23A03023-768x516-1.jpg"
+                            class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt=""
+                            decoding="async" /></noscript>
+                </div>
+                <div class="cont_wrap">
+                    <div class="date_cat flex_box al_center">
+                        <a href="https://sarchitects.com.vn/danh-muc-tin-tuc/tin-s-architects/">
+                            Tin S Architects </a>
+                        <div class="date">
+                            12/07/2023 </div>
+                    </div>
+                    <h3 class="font_oswald fw300 limit_line limit_5 fs_24 lh_34 text_uppercase m_0">Kỷ nguyên đô thị và
+                        những bước chuyển trong nghiên cứu đô thị ở Ðông Nam Á</h3>
+                </div>
+            </div>
+            <div class="item relative">
+                <a href="https://sarchitects.com.vn/tropical-space-dat-giai-thuong-monsoon-architecture-awards-tai-living-monsoon-lan-thu-2/"
+                    class="wrap_link"></a>
+                <div class="img_wrap">
+                    <img width="902" height="508"
+                        src="https://sarchitects.com.vn/wp-content/uploads/2023/07/23A05032-02-1.jpg"
+                        class="attachment-post-thumbnail size-post-thumbnail wp-post-image lazyloaded" alt=""
+                        decoding="async" data-ll-status="loaded"><noscript><img width="902" height="508"
+                            src="https://sarchitects.com.vn/wp-content/uploads/2023/07/23A05032-02-1.jpg"
+                            class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt=""
+                            decoding="async" /></noscript>
+                </div>
+                <div class="cont_wrap">
+                    <div class="date_cat flex_box al_center">
+                        <a href="https://sarchitects.com.vn/danh-muc-tin-tuc/tin-s-architects/">
+                            Tin S Architects </a>
+                        <div class="date">
+                            12/07/2023 </div>
+                    </div>
+                    <h3 class="font_oswald fw300 limit_line limit_5 fs_24 lh_34 text_uppercase m_0">Tropical Space đạt
+                        giải thưởng Monsoon Architecture Awards tại Living Monsoon lần thứ 2</h3>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
