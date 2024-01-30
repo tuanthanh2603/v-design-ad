1c5bd138f5d3c081f7270e0834bedb3cfd8b9ed2
tuanthanh-2 <tuanthanhktltk21@gmail.com>
2024-01-31 00:42:10 +0700

    done update details project

--- resources/views/user/pages/project/details.blade.php
+++ resources/views/user/pages/project/details.blade.php
@@ -62,8 +62,24 @@
                 <h2 class="sec_title_3">{{ $project->category->name }}</h2>
                 <ul class="ls_none m_0">
                     <li class="flex_box al_center">
-                        <span class="fs_14 lh_22 fw400">Hạng mục</span>
+                        <span class="fs_14 lh_22 fw400">Dòng sản phẩm</span>
                         <strong class="fs_14 lh_22 fw600">{{ $project->category->name }}</strong>
+                    </li>
+                    <li class="flex_box al_center">
+                        <span class="fs_14 lh_22 fw400">Kích thước</span>
+                        <strong class="fs_14 lh_22 fw600">Thiết kế theo yêu cầu</strong>
+                    </li>
+                    <li class="flex_box al_center">
+                        <span class="fs_14 lh_22 fw400">Khối lượng</span>
+                        <strong class="fs_14 lh_22 fw600">Thi công lắp đặt tận nơi</strong>
+                    </li>
+                    <li class="flex_box al_center">
+                        <span class="fs_14 lh_22 fw400">Nhà sản xuất</span>
+                        <strong class="fs_14 lh_22 fw600">V Design AD</strong>
+                    </li>
+                    <li class="flex_box al_center">
+                        <span class="fs_14 lh_22 fw400">Vận chuyển</span>
+                        <strong class="fs_14 lh_22 fw600">Miễn Phí Nội Thành TP. Hồ Chí Minh</strong>
                     </li>
                     @if($project->address)
                     <li class="flex_box al_center">
@@ -73,40 +89,11 @@
                     @endif
                 </ul>
                 <div class="box_content">
-                    <div class="fs_16 lh_22 fw600 text_3">Mô tả dự án</div>
+                    <div class="fs_16 lh_22 fw600 text_3">Chi tiết dự án</div>
                     <div class="fs_14 lh_24 fw400">
-                        <div class="content">
-                            <div class="content_inner">
-                                <p>{{ $project->description }}</p>
-                            </div>
-                        </div>
                         <div class="content hide">
                             <div class="share flex_box al_center">
-                                <span class="fs_13 lh_22 fw600">
-                                    CHIA SẺ </span>
-                                <a class="hover_zoom_img" href="http://www.facebook.com/sharer.php?u={{ url()->current() }}" target="_blank">
-                                    <svg class="hover_zoom" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M10 17.5C14.1421 17.5 17.5 14.1421 17.5 10C17.5 5.85786 14.1421 2.5 10 2.5C5.85786 2.5 2.5 5.85786 2.5 10C2.5 14.1421 5.85786 17.5 10 17.5Z" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M13.125 6.875H11.875C11.3777 6.875 10.9008 7.07254 10.5492 7.42418C10.1975 7.77581 10 8.25272 10 8.75V17.5" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M7.5 11.25H12.5" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </a>
-                                <a class="hover_zoom_img" href="http://twitter.com/share?text={{ $project->name }}&url={{ url()->current() }}" target="_blank">
-                                    <svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true" class="r-1nao33i r-4qtqp9 r-yyyyoo r-rxcuwo r-1777fci r-m327ed r-dnmrzs r-494qqr r-bnwqim r-1plcrui r-lrvibr">
-                                        <g>
-                                            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"></path>
-                                        </g>
-                                    </svg>
-                                </a>
-                                <a class="hover_zoom_img" href="https://www.linkedin.com/shareArticle?mini=true&url={{ url()->current() }}&title={{ $project->name }}" target="_blank">
-                                    <svg class="hover_zoom" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M16.25 3.125H3.75C3.40482 3.125 3.125 3.40482 3.125 3.75V16.25C3.125 16.5952 3.40482 16.875 3.75 16.875H16.25C16.5952 16.875 16.875 16.5952 16.875 16.25V3.75C16.875 3.40482 16.5952 3.125 16.25 3.125Z" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M9.375 8.75V13.75" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M6.875 8.75V13.75" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                        <path d="M6.875 6.95312C7.26333 6.95312 7.57812 6.63833 7.57812 6.25C7.57812 5.86167 7.26333 5.54688 6.875 5.54688C6.48667 5.54688 6.17188 5.86167 6.17188 6.25C6.17188 6.63833 6.48667 6.95312 6.875 6.95312Z" fill="#333333" />
-                                        <path d="M9.375 10.9375C9.375 10.3573 9.60547 9.80094 10.0157 9.3907C10.4259 8.98047 10.9823 8.75 11.5625 8.75C12.1427 8.75 12.6991 8.98047 13.1093 9.3907C13.5195 9.80094 13.75 10.3573 13.75 10.9375V13.75" stroke="#333333" stroke-linecap="round" stroke-linejoin="round" />
-                                    </svg>
-                                </a>
+                                <p>{{ $project->description }}</p>
                             </div>
                         </div>
                     </div>
@@ -120,13 +107,19 @@
                         </svg>
                     </div>
                 </div>
+                <div class="" style="margin-top: 1rem">
+                    <div class="load_more_projects btn_secondary" onclick="window.location.href='{{ url('/lien-he') }}'">
+                        Liên hệ tư vấn                    </div>
+                </div>
+
             </div>
+
         </div>
     </div>
 </section>
 <script>
     Fancybox.bind('[data-fancybox]', {
-        
-    });    
+
+    });
 </script>
 @endsection
